aa5d194a356c9014324157c654b90e385508c77a
Pimpli Bence <pimplibence@Pimplis-MacBook-Pro-2.local>
2020-07-14 21:32:26 +0200

    Fix bin error

--- docs/cli.md
+++ docs/cli.md
@@ -6,4 +6,4 @@ yarn global add @kilfy/beagle
 
 > **NOTE FOR**
 >
-> If you would like to serve the bin.js from node_modules, you can access it by **./node_modules/@kifly/beagle/src/bin/bin.js**
+> If you would like to serve the bin.ts from node_modules, you can access it by **./node_modules/@kifly/beagle/src/bin/bin.ts**
---
+++ src/bin/bin.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import * as yargs from 'yargs';
+import { Start } from './commands/start';
+
+yargs
+    .command('start', 'Start Application', (args) => Start.run(args))
+    .command('start-headless', 'Start Application Headless', (args) => Start.runHeadless(args))
+    .command('script', 'Run Application Script', (args) => Start.runScript(args));
+
+yargs
+    .options('c', {
+        alias: 'config',
+        default: './config.json',
+        describe: 'Specify custom config.json path',
+        type: 'string'
+    });
+
+// tslint:disable-next-line
+yargs.argv;
