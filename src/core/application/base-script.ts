e6db4ab85821cec1298c2400fba54af877bd88a1
Pimpli Bence <pimpli.bence@codebuild.hu>
2020-11-12 15:17:16 +0100

    Add app to baseScripts

--- src/core/application/base-script.ts
+++ src/core/application/base-script.ts
@@ -1,6 +1,13 @@
 import { Arguments } from 'yargs';
+import { BaseApplication } from './base-application';
 
 export abstract class BaseScript {
+    protected application: BaseApplication;
+
+    public setApplication(app: BaseApplication) {
+        this.application = app;
+    }
+
     public async run(args: Arguments<any>): Promise<void> {
         //
     }
