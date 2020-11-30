14e82095f25872a73969fe10a13346c986fce4c6
Pimpli Bence <pimpli.bence@gmail.com>
2020-11-30 21:57:34 +0100

    Remove unnecessary examples

--- examples/index.ts
+++ examples/index.ts
@@ -1,14 +1,9 @@
-import { BaseApplication, Provider } from '../src/core/application/base-application';
+import { BaseApplication } from '../src/core/application/base-application';
 import { appConfigurator } from '../src/core/application/decorators/app-configurator';
-import { FacebookService } from '../src/modules/facebook/facebook.service';
 
 export class Application extends BaseApplication {
-    protected providers: Provider[] = [
-        { injectable: FacebookService, options: { appSecret: 'c1b4fbe7cf13fa9edc94d5251c78a271' } }
-    ];
-
     @appConfigurator('*')
     public async hello() {
-        const facebook = this.container.resolve<FacebookService>(FacebookService);
+        console.log('Hello Bello');
     }
 }
