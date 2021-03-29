b8f119ac1bd6a599b4fab87a95a0615067ff5138
Bence Pimpli <pimpli.bence@codebuild.hu>
2021-03-29 16:29:45 +0200

    WIP

--- src/modules/codebuild/libs/adapter.ts
+++ src/modules/codebuild/libs/adapter.ts
@@ -34,4 +34,31 @@ export class Adapter {
             throw new InternalServerError('ICEUnknown', e);
         }
     }
+
+    public async requestWithHeaders(method: string, path: string, body: any, query: any = {}, headers = {}): Promise<any> {
+        const url = resolve(this.endpoint, path);
+
+        try {
+            const result = await superagent[method](url)
+                .set({
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    ...(headers || {})
+                })
+                .query(query)
+                .send(body);
+
+            return result.body;
+        } catch (e) {
+            if (e.errno === 'ECONNREFUSED') {
+                throw new InternalServerError('ICEConnection', e);
+            }
+
+            if (e?.response?.body) {
+                throw new InternalServerError(e?.response?.body?.message || 'ICEUnknownResponse', e?.response?.body?.payload);
+            }
+
+            throw new InternalServerError('ICEUnknown', e);
+        }
+    }
 }
