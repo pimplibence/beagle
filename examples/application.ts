87f9c5961cc71f678367f070b7cb59cb41b1a6f9
Bence Pimpli <pimpli.bence@codebuild.hu>
2021-12-13 15:27:26 +0100

    Upgrade libs

--- examples/application.ts
+++ examples/application.ts
@@ -1,24 +1,11 @@
 import { BaseApplication } from '../src/core/application/base-application';
 import { appConfigurator } from '../src/core/application/decorators/app-configurator';
-import { sleep } from '../src/modules/libs/sleep';
 import { applicationRunner } from '../src/runner/application-runner';
 
 export class Application extends BaseApplication {
     @appConfigurator()
     public async init() {
-        console.log('Hello Bello');
-    }
-
-    @appConfigurator()
-    public async init1() {
-        await sleep(1000);
-        console.log('Hello Bello');
-    }
-
-    @appConfigurator()
-    public async init2() {
-        await sleep(1000);
-        console.log('Hello Bello');
+        console.log('Hello Bello', this.environment);
     }
 }
 
