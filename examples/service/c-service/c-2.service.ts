a670a08e1fb7c5df7b6bd683763926c0a7fdbaa2
Bence Pimpli <pimplibence@gmail.com>
2022-03-01 20:31:24 +0100

    Remove unnecesary classes

--- examples/service/c-service/c-2.service.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from '../../../src/core/container/decorators/inject';
-import { injectable } from '../../../src/core/container/decorators/injectable';
-import { AService } from '../a.service';
-
-@injectable()
-export class C2Service {
-    @inject()
-    public a: AService;
-}
