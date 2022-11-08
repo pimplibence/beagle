a673965076eae8a0a9631b7d2be37bdc68a56e5b
Bence Pimpli <bencepimpli@bencepimpli.local>
2022-11-08 23:07:15 +0100

    Remove clutter

--- generators/plopfile.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node ./node_modules/plop/bin/plop.js
-const lodash = require('lodash');
-
-module.exports = (plop) => {
-    plop.setHelper('now', () => new Date());
-    plop.setHelper('package_json', (path) => lodash.get(require('../package.json'), path));
-    plop.setGenerator('application', require('./generators/application')(plop));
-}
